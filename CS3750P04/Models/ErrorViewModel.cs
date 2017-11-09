fb9e2b4116b0519e76b8438ffbdef8a13ca04d01
Noah Preszler <npreszler@marketstar.com>
2017-11-08 20:19:44 -0700

    Initial commit of the VS sln file and directory.

---
+++ CS3750P04/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CS3750P04.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
