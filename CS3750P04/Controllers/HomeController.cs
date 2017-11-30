cddadce078f56b8086a0927d2de4c4e8751005eb
Noah Preszler <npreszler@gmail.com>
2017-11-29 21:40:43 -0700

    Created a User action and view for the Home Controller. Currently shows all users in a table in the view as a learning/proof-of-concept thing.
    Added *.user to the gitignore

--- CS3750P04/Controllers/HomeController.cs
+++ CS3750P04/Controllers/HomeController.cs
@@ -12,85 +12,20 @@ namespace CS3750P04.Controllers
     {
         public IActionResult Index()
         {
-           // TimeTrackerEntityContext db = new TimeTrackerEntityContext("server=localhost;database=CS3750P04;user=student;password=picklerick");
-            //TimeTrackerEntityContext db = HttpContext.RequestServices.GetService(typeof(TimeTrackerEntityContext)) as TimeTrackerEntityContext;
-            //db.GetUsers();
-            //db.GetGroups();
-            //db.GetProjects();
-            //db.GetUserProjects();
+            TimeTrackerEntityContext db = HttpContext.RequestServices.GetService(typeof(TimeTrackerEntityContext)) as TimeTrackerEntityContext;
+            ViewData["Message"] = "This is a terrible test.";
+            var model = db.GetUsers();
+            
+            return View(model.FirstOrDefault());
+        }
 
-            //User user = db.GetUsers()[0];
+        public IActionResult User()
+        {
+            ViewData["Message"] = "The main screen with user info and metrics.";
+            TimeTrackerEntityContext db = HttpContext.RequestServices.GetService(typeof(TimeTrackerEntityContext)) as TimeTrackerEntityContext;
+            var model = db.GetUsers();
 
-            //user.FirstName = "new first name";
-
-            //Project proj = db.GetProjects()[0];
-
-            //proj.ProjectName = "updated name";
-
-            //db.updateProject(proj);
-
-            //Group group = db.GetGroups()[0];
-
-            //group.GroupName = "Updated Group Name";
-
-            //db.updateGroup(group);
-
-            //TimeEntry time = db.GetTimeEntries()[0];
-
-            //time.TimeStop = null;
-
-            //db.updateTimeEntry(time);
-
-           // db.updateUser(user);
-
-            //db.GetUsers();
-
-            //db.addUser(new Models.User()
-            //{
-            //    ScreenName = "Added from db",
-            //    FirstName = "Something",
-            //    LastName = "Special",
-            //    isActive = true,
-            //    UserHash = "Not implemeneted yet"
-            //});
-            //db.addProject(new Models.Project()
-            //{
-            //    ProjectName = "Test Project",
-            //    Active = true
-            //});
-
-            //db.GetUsers();
-            //db.GetProjects();
-
-            //db.addGroup(new Group()
-            //{
-            //    GroupName = "Test Group",
-            //    ProjectId = 1
-            //});
-
-            //db.GetGroups();
-
-            //db.addUserProject(new UserProject()
-            //{
-            //    UserId = 1,
-            //    GroupId = 1
-            //});
-
-            //db.GetUserProjects();
-
-            //db.addTimeEntry(new TimeEntry()
-            //{
-            //    TimeStart = DateTime.UtcNow,
-            //    UserId = 1,
-            //    CreateDate = DateTime.UtcNow,
-            //    Deleted = false,
-            //    EntryComment = "fadfda"
-            //});
-
-            //db.GetTimeEntries();
-
-            //return "hello from code?";
-            return View();
+            return View(model);
         }
 
         public IActionResult About()
